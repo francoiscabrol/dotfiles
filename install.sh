e44b2cfa4a046bc9bc4f97205c2f70f585806a5c
François Cabrol <francois@ffctn.com>
2014-05-15 15:38:35 -0400

    Now the install.sh works

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@
 ########## Variables
 
 DOTVIM_HOME=`pwd`
-dir=$DOTVIM_HOME/dotvim                    # dotfiles directory
-olddir=$DOTVIM_HOME/dotvim_old             # old dotfiles backup directory
+dir=$DOTVIM_HOME                              # dotfiles directory
+olddir=$DOTVIM_HOME/../dotvim_old             # old dotfiles backup directory
 files="vimrc gvimrc vim"    # list of files/folders to symlink in homedir
 
 ##########
@@ -26,7 +26,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir/
+    mv ~/.$file $olddir/$file
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
